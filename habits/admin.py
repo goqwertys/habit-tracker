d6f41aab98861776cf83398c45c9a264414f3fde
georgii <goqwertys@gmail.com>
2025-01-07 04:37:02 +0400

    Habit model added

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from habits.models import Habit
+
+
 # Register your models here.
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = (
+        'owner',
+        'place',
+        'start_time',
+        'action',
+        'is_pleasant',
+        'related_habit',
+        'frequency',
+        'reward',
+        'execution_time',
+        'is_public',
+    )
+    list_filter = (
+        'is_pleasant',
+        'is_public'
+    ),
+    search_fields = (
+        'owner',
+        'place',
+        'action',
+        'reward'
+    ),
+    ordering = ('id',)
--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,66 @@
+from datetime import timedelta
+
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+NULLABLE = {
+    'blank': True,
+    'null': True
+}
+
+
+class Habit(models.Model):
+    """ Habit model """
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='owner',
+        **NULLABLE
+    )
+    place = models.CharField(
+        max_length=100,
+        verbose_name='Place',
+        **NULLABLE
+    )
+    start_time = models.TimeField(
+        verbose_name='Start time',
+        help_text='Choose the time when you need to perform the habit',
+        **NULLABLE
+    )
+    action = models.CharField(
+        max_length=100,
+        verbose_name='The action of habit',
+        help_text='Specify the action of the habit',
+        **NULLABLE
+    )
+    is_pleasant = models.BooleanField(
+        default=False,
+        verbose_name='A sign of a pleasant habit'
+    )
+    related_habit = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        verbose_name='Related habit',
+        related_name='related_habits',
+        **NULLABLE
+    )
+    frequency = models.PositiveSmallIntegerField(
+        default=7,
+        verbose_name="Frequency of habit execution per week",
+        help_text="Specify the frequency of habit execution",
+    )
+    reward = models.CharField(
+        verbose_name='Reward after completing a habit'
+    )
+    execution_time = models.DurationField(
+        default=timedelta(seconds=120),
+        verbose_name='Time to do the habit'
+    )
+    is_public = models.BooleanField(
+        default=False,
+        verbose_name='Is habit public'
+    )
+    class Meta:
+        verbose_name = 'Habit'
+        verbose_name_plural = 'Habits'
