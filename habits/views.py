3f138e4f6f4f27d7d02e844c99e31b4a7753ee4b
georgii <goqwertys@gmail.com>
2025-01-07 20:07:25 +0400

    HabbitViewSet added

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import HabitPaginator
+from habits.serializers import HabitSerializer
+
+
+class HabitViewSet(ModelViewSet):
+    """ Habit CRUD """
+    queryset = Habit.objects.all()
+    serializer_class =HabitSerializer
+    pagination_class = HabitPaginator
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.save()
