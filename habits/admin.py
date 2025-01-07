c4c487bd20b7cf2dcf6ba13cc4efbd0d38b26a3b
georgii <goqwertys@gmail.com>
2025-01-07 04:42:18 +0400

    Habit model registered

--- habits/admin.py
+++ habits/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from habits.models import Habit
 
 
-# Register your models here.
 @admin.register(Habit)
 class HabitAdmin(admin.ModelAdmin):
     list_display = (
@@ -21,11 +20,11 @@ class HabitAdmin(admin.ModelAdmin):
     list_filter = (
         'is_pleasant',
         'is_public'
-    ),
+    )
     search_fields = (
         'owner',
         'place',
         'action',
         'reward'
-    ),
+    )
     ordering = ('id',)
