4c93c3cc151f7a401509a7c156ea9aa254692ac0
georgii <goqwertys@gmail.com>
2025-01-07 20:07:04 +0400

    HabitSerializer added

---
+++ habits/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import ModelSerializer
+
+from habits.models import Habit
+
+
+class HabitSerializer(ModelSerializer):
+    """ Habit serializer"""
+    class Meta:
+        model = Habit
+        fields = '__all__'
+        validators = []
