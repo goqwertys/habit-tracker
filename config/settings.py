abf6dba7276efd732ceff54ded40d76c55407bdb
georgii <goqwertys@gmail.com>
2025-01-10 21:18:18 +0400

    drf-yasg set up

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'django_celery_beat',
     'corsheaders',
+    'drf_yasg',
 
     'users',
     'habits',
--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Habit tracker API",
+      default_version='v0.1',
+      description="Every action you take is a vote for the person you want to become",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="goqwertys@gmail.com", name="Author"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny,]
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('habits/', include('habits.urls', namespace='habits')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
 
     # docs
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
