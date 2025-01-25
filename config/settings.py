06dff2d9d20592f43a671d4f683b6b010110f77c
georgii <goqwertys@gmail.com>
2025-01-25 23:52:54 +0400

    MEDIA_URL and MEDIA_ROOT added

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
