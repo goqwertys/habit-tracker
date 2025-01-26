d3bab1e63aa71f7e5e36aca01724a482ff63aadf
georgii <goqwertys@gmail.com>
2025-01-26 17:32:34 +0400

    DurationFieldInSeconds serializer added for timedelta

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -13,11 +15,38 @@ class RelatedHabitSerializer(ModelSerializer):
         fields = '__all__'
 
 
+class DurationFieldInSeconds(serializers.DurationField):
+    def to_representation(self, value):
+        if not value:
+            return None
+
+        # Convert timedelta to a string of the format "HH:MM:SS"
+        total_seconds = int(value.total_seconds())
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+    def to_internal_value(self, data):
+        # Parse a string of the format "HH:MM:SS" in timedelta
+        try:
+            parts = list(map(int, data.split(':')))
+            if len(parts) == 3:
+                return timedelta(hours=parts[0], minutes=parts[1], seconds=parts[2])
+            elif len(parts) == 2:
+                return timedelta(minutes=parts[0], seconds=parts[1])
+            raise serializers.ValidationError("Invalid time format. Use 'HH:MM:SS' or 'MM:SS'")
+        except (ValueError, TypeError):
+            raise serializers.ValidationError("Invalid time format. Use numbers separated by colons")
+
+
 class HabitSerializer(serializers.ModelSerializer):
-    # Для чтения данных
+    execution_time = DurationFieldInSeconds(
+        help_text="Format: HH:MM:SS or MM:SS (eg: 00:02:00 for 2 minutes)"
+    )
+    # To read data
     related_habit = RelatedHabitSerializer(read_only=True)
 
-    # Для записи данных
+    # To record data
     related_habit_id = serializers.PrimaryKeyRelatedField(
         queryset=Habit.objects.all(),
         source='related_habit',
